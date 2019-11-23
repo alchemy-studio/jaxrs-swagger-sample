9454dd2bb52545c4f330ad51327fa76606af74fc
moicen <i@moicen.com>
2019-11-23 17:58:56 +0800

    add test & circleci config

--- src/test/java/cn/alchemystudio/swagger/jaxrs/JaxrsSwaggerSampleApplicationTests.java
+++ src/test/java/cn/alchemystudio/swagger/jaxrs/JaxrsSwaggerSampleApplicationTests.java
@@ -1,13 +1,24 @@
 package cn.alchemystudio.swagger.jaxrs;
 
+import cn.alchemystudio.swagger.jaxrs.common.HttpWebResponse;
+import cn.alchemystudio.swagger.jaxrs.rest.DummyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class JaxrsSwaggerSampleApplicationTests {
 
     @Test
-    void contextLoads() {
+    void dummyTest() {
+        DummyService dummyService = new DummyService();
+        HttpWebResponse res = dummyService.get();
+        assertTrue(res.isSuccess());
+        assertEquals("dummy", res.getMessage());
+        assertNull(res.getResult());
     }
 
 }
